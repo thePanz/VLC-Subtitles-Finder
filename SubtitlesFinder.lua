26b4c6a10721374e8c0cc71be3fc7c104b492a0c
Ema <thepanz@gmail.com>
2012-05-13 16:16:21 +0200

    Added Romanian search language

--- SubtitlesFinder.lua
+++ SubtitlesFinder.lua
@@ -693,6 +693,7 @@ languages = {
 	{ title = "Japanese", tag = "ja" },
 	{ title = "Polish", tag = "pl" },
 	{ title = "Portuguese", tag = "pt" },
+	{ title = "Romanian", tag = "ron" },
 	{ title = "Russian", tag = "ru" },
 	{ title = "Spanish", tag = "es" }
 }
